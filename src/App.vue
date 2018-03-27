3e0ce814b4fa127f5f8e8c35dd8bac233a79c2b9
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-03-27 22:28:27 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  name: 'App'
+}
+</script>
