927b3bbd0a08799d4674678998831634412830d8
Slavik <slavik.sukhanov@iskyweb.com>
2018-03-28 10:35:11 +0300

    created Toolbar, Navigation menu for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <v-app>
-    <v-toolbar>
+    <!-- Navigation drawer -->
+    <v-navigation-drawer v-model='sideNav' absolute temporary>
+      <v-list>
+        <v-list-tile href="javascript:;">
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Toolbar -->
+    <v-toolbar class='red white--text'>
+      <v-toolbar-side-icon class='hidden-sm-and-up white--text' @click='sideNav = !sideNav' />
       <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items class='hidden-xs-only'>
+        <v-btn flat class='white--text'>
+          <v-icon left>supervisor_account</v-icon>
+          View meetups
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
+    
   </v-app>
 </template>
 
@@ -10,6 +32,7 @@
 export default {
   data () {
     return {
+      sideNav: false
     }
   },
   name: 'App'
