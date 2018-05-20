59315d84018c0c4b40a7f8e75b6a579cf28cebb0
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2018-05-20 10:51:41 +0300

    Store: rewritten with  async/await

--- src/store/meetups/index.js
+++ src/store/meetups/index.js
@@ -1,143 +1,132 @@
 import * as firebase from 'firebase'
 
-export default {
-  state: {
-    loadedMeetups: []
+const state = {
+  loadedMeetups: []
+}
+
+const getters = {
+  loadedMeetups (state) {
+    return state.loadedMeetups.sort((meetupA, meetupB) => {
+      return meetupA.data > meetupB.data
+    })
   },
-  getters: {
-    loadedMeetups (state) {
-      return state.loadedMeetups.sort((meetupA, meetupB) => {
-        return meetupA.data > meetupB.data
+  loadedMeetup (state) {
+    return (meetupId) => {
+      return state.loadedMeetups.find((meetup) => {
+        return meetup.id === meetupId
       })
-    },
-    loadedMeetup (state) {
-      return (meetupId) => {
-        return state.loadedMeetups.find((meetup) => {
-          return meetup.id === meetupId
-        })
-      }
-    },
-    featuredMeetups (state, getters) {
-      return getters.loadedMeetups.slice(0, 5)
     }
   },
-  mutations: {
-    setLoadMeetups (state, meetups) {
-      state.loadedMeetups = meetups
-    },
-    createMeetup (state, meetup) {
-      state.loadedMeetups.push(meetup)
-    },
-    updateMeetup (state, meetupData) {
-      const meetup = state.loadedMeetups.find(meetup => {
-        return meetup.id === meetupData.id
-      })
+  featuredMeetups (state, getters) {
+    return getters.loadedMeetups.slice(0, 5)
+  }
+}
 
-      if (meetupData.title) {
-        meetup.title = meetupData.title
-      }
-      if (meetupData.description) {
-        meetup.description = meetupData.description
-      }
-      if (meetupData.date) {
-        meetup.date = meetupData.date
-      }
-    }
+const mutations = {
+  setLoadMeetups (state, meetups) {
+    state.loadedMeetups = meetups
   },
-  actions: {
-    loadMeetups ({ commit }) {
-      commit('setLoading', true)
-      firebase.database().ref('meetups').once('value')
-        .then((data) => {
-          const meetups = []
-          const obj = data.val()
+  createMeetup (state, meetup) {
+    state.loadedMeetups.push(meetup)
+  },
+  updateMeetup (state, meetupData) {
+    const meetup = state.loadedMeetups.find(meetup => {
+      return meetup.id === meetupData.id
+    })
 
-          for (let key in obj) {
-            meetups.push({
-              id: key,
-              date: obj[key].date,
-              description: obj[key].description,
-              imageUrl: obj[key].imageUrl,
-              location: obj[key].location,
-              title: obj[key].title,
-              creatorId: obj[key].creatorId
-            })
-          }
-
-          commit('setLoadMeetups', meetups)
-          commit('setLoading', false)
-        })
-        .catch(error => {
-          console.log(error)
-          commit('setLoading', false)
-        })
-    },
-    createMeetup ({ commit, getters }, meetupData) {
-      console.log(meetupData.date)
-      const meetup = {
-        title: meetupData.title,
-        location: meetupData.location,
-        description: meetupData.description,
-        date: meetupData.date.toISOString(),
-        creatorId: getters.user.id
-      }
-
-      let imageUrl
-      let key
-
-      firebase.database().ref('meetups').push(meetup)
-        .then(data => {
-          key = data.key
-          return key
-        })
-        .then(key => {
-          const filename = meetupData.image.name
-          const ext = filename.slice(filename.lastIndexOf('.'))
-          return firebase.storage().ref('meetups/' + key + ext).put(meetupData.image)
-        })
-        .then(fileData => {
-          imageUrl = fileData.metadata.downloadURLs[0]
-          return firebase.database().ref('meetups').child(key).update({imageUrl: imageUrl})
-        })
-        .then(() => {
-          commit('createMeetup', {
-            ...meetup,
-            imageUrl: imageUrl,
-            id: key
-          })
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    updateMeetupData ({ commit }, meetupData) {
-      commit('setLoading', true)
-      const meetupObj = {}
-
-      if (meetupData.title) {
-        meetupObj.title = meetupData.title
-      }
-      if (meetupData.description) {
-        meetupObj.description = meetupData.description
-      }
-      if (meetupData.date) {
-        meetupObj.date = meetupData.date.toISOString()
-        meetupData.date = meetupData.date.toISOString()
-      }
-
-      firebase
-        .database()
-        .ref('meetups')
-        .child(meetupData.id)
-        .update(meetupObj)
-        .then(() => {
-          commit('setLoading', false)
-          commit('updateMeetup', meetupData)
-        })
-        .catch(error => {
-          console.log(error)
-          commit('setLoading', false)
-        })
+    if (meetupData.title) {
+      meetup.title = meetupData.title
+    }
+    if (meetupData.description) {
+      meetup.description = meetupData.description
+    }
+    if (meetupData.date) {
+      meetup.date = meetupData.date
     }
   }
+}
+
+const actions = {
+  async loadMeetups ({ commit }) {
+    commit('setLoading', true)
+
+    try {
+      const meetups = []
+      const data = await firebase.database().ref('meetups').once('value')
+      const dirtyMeetups = data.val()
+
+      for (let key in dirtyMeetups) {
+        meetups.push({
+          id: key,
+          ...dirtyMeetups[key]
+        })
+      }
+
+      commit('setLoadMeetups', meetups)
+      commit('setLoading', false)
+    } catch (error) {
+      console.log(error)
+      commit('setLoading', false)
+    }
+  },
+  async createMeetup ({ commit, getters }, meetupData) {
+    const meetup = {
+      title: meetupData.title,
+      location: meetupData.location,
+      description: meetupData.description,
+      date: meetupData.date.toISOString(),
+      creatorId: getters.user.id
+    }
+
+    try {
+      const data = await firebase.database().ref('meetups').push(meetup)
+      const key = data.key
+
+      const filename = meetupData.image.name
+      const ext = filename.slice(filename.lastIndexOf('.'))
+      const fileData = await firebase.storage().ref('meetups/' + key + ext).put(meetupData.image)
+
+      const imageUrl = fileData.metadata.downloadURLs[0]
+      await firebase.database().ref('meetups').child(key).update({imageUrl: imageUrl})
+
+      commit('createMeetup', {
+        id: key,
+        imageUrl,
+        ...meetup
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  async updateMeetupData ({ commit }, meetupData) {
+    commit('setLoading', true)
+    const meetupObj = {}
+
+    if (meetupData.title) {
+      meetupObj.title = meetupData.title
+    }
+    if (meetupData.description) {
+      meetupObj.description = meetupData.description
+    }
+    if (meetupData.date) {
+      meetupObj.date = meetupData.date.toISOString()
+      meetupData.date = meetupData.date.toISOString()
+    }
+
+    try {
+      await firebase.database().ref('meetups').child(meetupData.id).update(meetupObj)
+      commit('setLoading', false)
+      commit('updateMeetup', meetupData)
+    } catch (error) {
+      console.log(error)
+      commit('setLoading', false)
+    }
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
 }
